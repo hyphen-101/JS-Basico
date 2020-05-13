9da395508e075fe86cc81e2b64688f0642140720
hyphen-101 <semicodi.smcd@gmail.com>
2020-05-13 12:58:33 -0500

    actualizacion

---
+++ js/scripts01.js
@@ -0,0 +1,18 @@
+// En java script existen 3 formas de crear variables
+// Como escribit las variables y variable VAR**
+//  var nombre;
+// var nombreCliente = 'Pedro' //Cammel case
+// var nombre_cliente = 'Juan' // underscore
+// var NombreCliente = 'Fabiolo' // Pascal case
+
+
+// console.log(nombre);
+/* la similitud entre var y let es que te permite declarar
+   variables sin valor; despues se le pueden añadir.
+   una diferencia es let no puede re-declarar variables mientra
+   var sí*/
+
+// let nombre;
+// nombre = 'Kare';
+// let nombre = 'juan'; // esto es error de sintaxis
+// console.log(nombre);
